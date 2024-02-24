829a263462eb25065bb542dffadd6d32f2cdbdd6
Tutuacs <arthursilva.rs1@gmail.com>
2024-02-23 21:10:00 -0300

    a16

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -12,5 +12,34 @@
 // * The locker assignment should use an Option<i32>
 
 
+struct Student {
+    name: String,
+    locker: Option<i32>
+}
 
-fn main() {}
+impl Student {
+    fn new(name: String, locker: Option<i32>) -> Self {
+        Self {
+            name,
+            locker,
+        }
+    }
+
+    fn print(&self) {
+        println!("\nStudents name: {}", self.name);
+        match &self.locker {
+            Some(number) => println!("the student have locker: {:?}", number), 
+            None => println!("the student dont have"), 
+        }
+    }
+}
+
+
+fn main() {
+
+    let name: String = "Arthur".into();
+    let locker = Some(3);
+    let student: Student = Student::new(name, locker);
+
+    student.print();
+}
