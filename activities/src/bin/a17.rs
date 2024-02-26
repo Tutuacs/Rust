600cf2ac9b44201f8f631982da7d346bd4f51bde
Tutuacs <arthursilva.rs1@gmail.com>
2024-02-25 22:54:40 -0300

    a17

--- activities/src/bin/a17.rs
+++ activities/src/bin/a17.rs
@@ -12,4 +12,14 @@
 //   to uppercase and lowercase
 //   * Try searching for: to_uppercase, to_lowercase
 
-fn main() {}
+fn main() {
+
+    let my_string = "Minha String".to_owned();
+    
+    let my_upper_string = my_string.to_uppercase();
+    let my_lower_string = my_string.to_lowercase();
+
+    println!("\nNormal: {}",my_string);
+    println!("\nUpper: {}",my_upper_string);
+    println!("\nLower: {}",my_lower_string);
+}
